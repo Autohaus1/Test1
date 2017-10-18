8e1d4791e1c1d50cc3e60ab78873caca90d5bbfe
Autohaus1 <fam-zelke@unitybox.de>
2017-10-18 16:50:29 +0200

    Abschluss Tag 1 UI5

--- webapp/view/App.view.js
+++ webapp/view/App.view.js
@@ -1,47 +1,68 @@
 sap.ui.jsview("view.App", {
 
 	/** Specifies the Controller belonging to this View. 
-	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	* @memberOf view.App
-	*/ 
-	getControllerName : function() {
+	 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	 * @memberOf view.App
+	 */
+	getControllerName: function() {
 		return "controller.App";
 	},
 
 	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
-	* Since the Controller is given to this method, its event handlers can be attached right away. 
-	* @memberOf view.App
-	*/ 
-	createContent : function(oController) {
-		
+	 * Since the Controller is given to this method, its event handlers can be attached right away. 
+	 * @memberOf view.App
+	 */
+	createContent: function(oController) {
+
+		var oText1 = new sap.m.Text({
+			text: "{path: 'dataModel>/value1'}"   // identisch zu  
+		});
+
 		var oSlider1 = new sap.m.Slider({
-		        change: oController.onSliderChange 		
-	        	});
-	 
+			value: "{dataModel>/value1}",         // identisch zu  
+			change: oController.onSliderChange
+		});
+
+		var oText2 = new sap.m.Text({
+			text: "{model: 'dataModel', path: '/value2'}"    // identisch zu  beste value: - Zuweisung
+		});
+ 
 		var oSlider2 = new sap.m.Slider({
-		        change: oController.onSliderChange 		
-	        	});
-	
+			value: "{model: 'dataModel', path: '/value2'}",   // identisch zu  
+			change: oController.onSliderChange
+		});
+
 		var oButton = new sap.m.Button({
-		  tooltip:"Ganz schön schwer ", 
-	    	text:" Blödsinn",
-	    	press: function() {
-	     		alert("Button gedrückt");
-		    }
-		   });
-     
+			tooltip: "Ganz schön schwer ",
+			text: " Blödsinn",
+			press: function() {
+				alert("Button gedrückt");
+			}
+		});
+
 		var oButton2 = new sap.m.Button({
-		   tooltip:"Ganz schön leicht",
-		   text:"nicht Blödsinn"
-		   });
-      
-	 		return new sap.m.Page({
+			tooltip: "Ganz schön leicht",
+			text: "nicht Blödsinn"
+		});
+
+		var oVLayout = new sap.m.VBox({
+			width: "90rem",
+			items: [
+				oSlider1,
+				oText1,
+				oSlider2,
+				oText2,
+				oButton,
+				oButton2
+
+			]
+
+		});
+
+		return new sap.m.Page({
 			title: "Meine 1. APP",
-			content: [oSlider1,
-			          oSlider2,
-			          oButton,
-			          oButton2
-			
+			content: [oVLayout
+
 			]
 		});
 	}
