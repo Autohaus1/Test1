3d29d200951296f10a99f9f0e1fbbced121d9408
Autohaus1 <fam-zelke@unitybox.de>
2017-10-19 11:10:26 +0200

    Spracheineinstellung gepflegt

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,33 +1,38 @@
 sap.ui.define([
-		"sap/ui/core/UIComponent"
-	], function (UIComponent) {
-		"use strict";
+	"sap/ui/core/UIComponent"
+], function(UIComponent) {
+	"use strict";
 
-		return UIComponent.extend("de.Test_OSZ.schul02.Component", {
+	return UIComponent.extend("de.Test_OSZ.schul02.Component", {
 
-			metadata : {
-				name: "Meine App",
-				version: "0.5",
-				dependencies:  {
-					libs: ["sap.m"]
-				},
-				rootView: {
-					viewName: "de.Test_OSZ.schul02.view.App",
-					type: "XML"
-				}
+		metadata: {
+			name: "Meine App",
+			version: "0.5",
+			dependencies: {
+				libs: ["sap.m"]
 			},
-			init : function () {
-				UIComponent.prototype.init.apply(this, arguments);
-					var oData = {
-			value1: 50,
-			value2: 90
-		};
-
-		var oModel = new sap.ui.model.json.JSONModel(oData); // Variable Model anlegen
-
-		this.setModel(oModel, "dataModel"); // Model einbinden  dadurch Model in der App überall verfügbar
-				
+			rootView: {
+				viewName: "de.Test_OSZ.schul02.view.App",
+				type: "XML"
 			}
-		});
-	}
-);
+		},
+		init: function() {
+			UIComponent.prototype.init.apply(this, arguments);
+
+			var i18nModel = new sap.ui.model.resource.ResourceModel({     
+				bundleUrl: "i18n/i18n.properties"
+			});
+			this.setModel(i18nModel, "i18n");
+			
+			var oData = {
+				value1: 50,
+				value2: 90
+			};
+
+			var oModel = new sap.ui.model.json.JSONModel(oData); // Variable Model anlegen
+
+			this.setModel(oModel, "dataModel"); // Model einbinden  dadurch Model in der App überall verfügbar
+
+		}
+	});
+});
